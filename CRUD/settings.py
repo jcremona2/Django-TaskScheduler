c845963770e191745371a019250ebf2565fc655b
Julian Cremona <jncremona@gmail.com>
2023-01-22 03:27:00 -0300

    test

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'CRUD.wsgi.application'
 #    }
 #}
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite',        conn_max_age=600    )}
+    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost/postgres',        conn_max_age=600    )}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
